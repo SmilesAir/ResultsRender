52b800b785fc2b12e9b2ba1c4dcd13ae7f2f14f9
Ryan Young <ryan@smilesair.com>
2022-12-26 20:36:26 -0800

    Prod endpoints

--- client/source/endpoints.js
+++ client/source/endpoints.js
@@ -1,11 +1,23 @@
 "use strict"
 
-let urls = {
-    GET_PLAYER_DATA: "https://tkhmiv70u9.execute-api.us-west-2.amazonaws.com/development/getAllPlayers",
-    GET_EVENT_DATA: "https://xyf6qhiwi1.execute-api.us-west-2.amazonaws.com/development/getAllEvents",
-    GET_RESULTS_DATA: "https://pkbxpw400j.execute-api.us-west-2.amazonaws.com/development/getAllResults",
-    GET_POINTS_MANIFEST: "https://k7p1y5ntz6.execute-api.us-west-2.amazonaws.com/development/getManifest",
-    GET_POINTS_DATA: "https://k7p1y5ntz6.execute-api.us-west-2.amazonaws.com/development/downloadPointsData/<key>"
+
+let urls = undefined
+if (__STAGE__ === "DEVELOPMENT") {
+    urls = {
+        GET_PLAYER_DATA: "https://tkhmiv70u9.execute-api.us-west-2.amazonaws.com/development/getAllPlayers",
+        GET_EVENT_DATA: "https://xyf6qhiwi1.execute-api.us-west-2.amazonaws.com/development/getAllEvents",
+        GET_RESULTS_DATA: "https://pkbxpw400j.execute-api.us-west-2.amazonaws.com/development/getAllResults",
+        GET_POINTS_MANIFEST: "https://k7p1y5ntz6.execute-api.us-west-2.amazonaws.com/development/getManifest",
+        GET_POINTS_DATA: "https://k7p1y5ntz6.execute-api.us-west-2.amazonaws.com/development/downloadPointsData/<key>"
+    }
+} else {
+    urls = {
+        GET_PLAYER_DATA: "https://4wnda3jb78.execute-api.us-west-2.amazonaws.com/production/getAllPlayers",
+        GET_EVENT_DATA: "https://wyach4oti8.execute-api.us-west-2.amazonaws.com/production/getAllEvents",
+        GET_RESULTS_DATA: "https://v869a98rf9.execute-api.us-west-2.amazonaws.com/production/getAllResults",
+        GET_POINTS_MANIFEST: "https://kvq5a3et4b.execute-api.us-west-2.amazonaws.com/production/getManifest",
+        GET_POINTS_DATA: "https://kvq5a3et4b.execute-api.us-west-2.amazonaws.com/production/downloadPointsData/<key>"
+    }
 }
 
 module.exports.buildUrl = function(key, pathParams, queryParams) {
